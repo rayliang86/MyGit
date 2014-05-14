235dde5d8c74d76f3b89a3298293cf1ade25eb3c
rayliang86 <rayliang86@users.noreply.github.com>
2014-05-14 14:44:24 +0800

    Update raylianghah.java

--- test/src/test/raylianghah.java
+++ test/src/test/raylianghah.java
@@ -5,5 +5,8 @@ public class raylianghah
 	public static void main(String[] args)
 	{
 		System.out.println("I belong to branch usage!");
+		
+		System.out.println("Changed directly from github!");
+		
 	}
 }
